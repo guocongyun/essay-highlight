7de598b57732e3229c0ccab9298b381bfeca96f6
Congyun Guo <guocongyun1218@hotmail.com>
2021-09-13 17:19:01 +0800

    Fix package issue with sentence transformers

--- api/allennlp_demo/bidaf_elmo/api.py
+++ api/allennlp_demo/bidaf_elmo/api.py
@@ -3,7 +3,7 @@ import os
 from allennlp_demo.common import config, http
 
 
-class BidafElmoModelEndpoint(http.ModelEndpoint):
+class BidafElmoModelEndpoint(http.MyModelEndpoint):
     def __init__(self):
         c = config.Model.from_file(os.path.join(os.path.dirname(__file__), "model.json"))
         super().__init__(c)
